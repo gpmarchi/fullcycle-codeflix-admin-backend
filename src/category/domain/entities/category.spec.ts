ab05e1432af749525802bb64b3ab14786d2bfb55
gpmarchi <gustavomarchi@gmail.com>
2023-02-03 14:11:33 -0300

    feat: create category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,9 +1,16 @@
-import { Category } from "./category";
+import { Category } from './category';
 
-describe('Categories', () => {
+describe('Category', () => {
   it('should be able to create a new category', () => {
-    const category = new Category('Movie');
+    const categoryProps = {
+      name: 'Movie',
+      description: 'some description',
+      isActive: true,
+      createdAt: new Date(),
+    };
 
-    expect(category.name).toBe('Movie');
-   });
+    const category = new Category(categoryProps);
+
+    expect(category.props).toStrictEqual(categoryProps);
+  });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,30 @@
+export interface ICategoryProps {
+  name: string;
+  description?: string;
+  isActive: boolean;
+  createdAt?: Date;
+}
+
 export class Category {
-  constructor(public name: string) {}
+  constructor(public readonly props: ICategoryProps) {}
+
+  public get name(): string {
+    return this.props.name;
+  }
+
+  public get description(): string | undefined {
+    return this.props.description;
+  }
+
+  public get value(): boolean {
+    return this.props.isActive;
+  }
+
+  public get isActive(): boolean {
+    return this.props.isActive;
+  }
+
+  public get createdAt(): Date | undefined {
+    return this.props.createdAt;
+  }
 }
