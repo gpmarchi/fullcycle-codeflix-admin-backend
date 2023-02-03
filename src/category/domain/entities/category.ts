05c04ed065d1213131b482939f58539eb6f4c048
gpmarchi <gustavomarchi@gmail.com>
2023-02-03 16:06:30 -0300

    feat: add setters to category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,30 +1,56 @@
+import { type Replace } from 'helpers/Replace';
+
 export interface ICategoryProps {
   name: string;
-  description?: string;
+  description?: string | null;
   isActive: boolean;
-  createdAt?: Date;
+  createdAt: Date;
 }
 
 export class Category {
-  constructor(public readonly props: ICategoryProps) {}
+  private readonly props: ICategoryProps;
+
+  constructor(
+    props: Replace<
+      ICategoryProps,
+      {
+        isActive?: boolean;
+        createdAt?: Date;
+      }
+    >,
+  ) {
+    this.props = {
+      ...props,
+      isActive: props.isActive ?? true,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
 
   public get name(): string {
     return this.props.name;
   }
 
-  public get description(): string | undefined {
+  public set name(name: string) {
+    this.props.name = name;
+  }
+
+  public get description(): string | null | undefined {
     return this.props.description;
   }
 
-  public get value(): boolean {
-    return this.props.isActive;
+  public set description(description: string | null | undefined) {
+    this.props.description = description;
   }
 
   public get isActive(): boolean {
     return this.props.isActive;
   }
 
-  public get createdAt(): Date | undefined {
+  public set isActive(isActive: boolean) {
+    this.props.isActive = isActive;
+  }
+
+  public get createdAt(): Date | null | undefined {
     return this.props.createdAt;
   }
 }
