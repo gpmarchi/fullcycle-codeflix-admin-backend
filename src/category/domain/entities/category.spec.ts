378094d53d3165b253d0c44e9e20b6015c743abe
gpmarchi <gustavomarchi@gmail.com>
2023-02-06 12:15:59 -0300

    test: finalize category creation tests

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -60,4 +60,50 @@ describe('Category', () => {
     expect(category.createdAt).toBeInstanceOf(Date);
     expect(category.createdAt).toBe(date);
   });
+
+  it('should be able to get and set contents of name field', () => {
+    const categoryProps = {
+      name: 'Movie',
+      description: 'some description',
+    };
+
+    const category = new Category(categoryProps);
+
+    expect(category.name).toBe(categoryProps.name);
+
+    category.name = 'new name';
+
+    expect(category.name).toBe('new name');
+  });
+
+  it('should be able to get and set contents of description field', () => {
+    const categoryProps = {
+      name: 'Movie',
+      description: 'some description',
+    };
+
+    const category = new Category(categoryProps);
+
+    expect(category.description).toBe(categoryProps.description);
+
+    category.description = 'new description';
+
+    expect(category.description).toBe('new description');
+  });
+
+  it('should be able to get and set contents of isActive field', () => {
+    const categoryProps = {
+      name: 'Movie',
+      description: 'some description',
+      isActive: true,
+    };
+
+    const category = new Category(categoryProps);
+
+    expect(category.isActive).toBe(categoryProps.isActive);
+
+    category.isActive = false;
+
+    expect(category.isActive).toBe(false);
+  });
 });
