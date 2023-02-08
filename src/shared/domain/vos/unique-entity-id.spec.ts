5bbbfe155a0cfec501f212ae8c2873617903ce3d
gpmarchi <gustavomarchi@gmail.com>
2023-02-08 16:38:43 -0300

    test: add more test cases to suite

--- src/shared/domain/vos/unique-entity-id.spec.ts
+++ src/shared/domain/vos/unique-entity-id.spec.ts
@@ -9,9 +9,23 @@ describe('UniqueEntityId', () => {
     expect(validateSpy).toHaveBeenCalled();
   });
 
-  it('should get an unique entity id from instance', () => {
+  it('should get an unique entity id from new instance', () => {
     const uniqueEntityId = new UniqueEntityId();
 
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+
     expect(uniqueEntityId.id).toBeTruthy();
+    expect(validateSpy).toHaveBeenCalled();
+  });
+
+  it('should create new instance with custom uuid', () => {
+    const uuid = '6b58dc97-2ef3-4bb6-9b9d-d3c11a526033';
+
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+
+    const uniqueEntityId = new UniqueEntityId(uuid);
+
+    expect(uniqueEntityId.id).toBe(uuid);
+    expect(validateSpy).toHaveBeenCalled();
   });
 });
