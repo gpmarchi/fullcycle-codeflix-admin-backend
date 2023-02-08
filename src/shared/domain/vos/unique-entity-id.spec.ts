7a9daac59e2ed5a01c79292f892c36270c71816f
gpmarchi <gustavomarchi@gmail.com>
2023-02-08 16:53:52 -0300

    test: extract validate method spy to shared variable for reuse across tests

--- src/shared/domain/vos/unique-entity-id.spec.ts
+++ src/shared/domain/vos/unique-entity-id.spec.ts
@@ -1,10 +1,10 @@
 import { InvalidUUIDError } from '../../../shared/errors/invalid-uuid';
 import { UniqueEntityId } from './unique-entity-id';
 
+const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
+
 describe('UniqueEntityId', () => {
   it('should throw an error when uuid is invalid', () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
-
     expect(() => new UniqueEntityId('fake id')).toThrow(InvalidUUIDError);
     expect(validateSpy).toHaveBeenCalled();
   });
@@ -12,16 +12,12 @@ describe('UniqueEntityId', () => {
   it('should get an unique entity id from new instance', () => {
     const uniqueEntityId = new UniqueEntityId();
 
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
-
     expect(uniqueEntityId.id).toBeTruthy();
     expect(validateSpy).toHaveBeenCalled();
   });
 
   it('should create new instance with custom uuid', () => {
     const uuid = '6b58dc97-2ef3-4bb6-9b9d-d3c11a526033';
-
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate');
 
     const uniqueEntityId = new UniqueEntityId(uuid);
 
