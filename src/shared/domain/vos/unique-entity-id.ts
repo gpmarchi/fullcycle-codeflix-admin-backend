b1fcdd2b3b4741b80b0685caba5ceccfca18ccce
gpmarchi <gustavomarchi@gmail.com>
2023-02-08 15:03:18 -0300

    feat: add getter method

--- src/shared/domain/vos/unique-entity-id.ts
+++ src/shared/domain/vos/unique-entity-id.ts
@@ -3,10 +3,10 @@ import { randomUUID } from 'crypto';
 import { InvalidUUIDError } from '../../../shared/errors/invalid-uuid';
 
 export class UniqueEntityId {
-  private readonly id: string;
+  private readonly _id: string;
 
   constructor(id?: string) {
-    this.id = id ?? randomUUID();
+    this._id = id ?? randomUUID();
     this.validate();
   }
 
@@ -14,10 +14,14 @@ export class UniqueEntityId {
     const regex =
       /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
 
-    const isValid = regex.test(this.id);
+    const isValid = regex.test(this._id);
 
     if (!isValid) {
       throw new InvalidUUIDError();
     }
+  }
+
+  public get id(): string {
+    return this._id;
   }
 }
